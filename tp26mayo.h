604d7150cc6bad20534d6aad4ef4d8c5da47ed70
benshiii <107224857+benshiii@users.noreply.github.com>
2025-05-25 22:14:33 -0300

    Create tp26mayo.h

---
+++ tp26mayo.h
@@ -0,0 +1,22 @@
+#ifndef LISTA_H
+#define LISTA_H
+
+typedef struct Nodo
+{
+    char nombre[256]; // Ahora almacena toda la línea
+    struct Nodo *sig;
+} Nodo;
+
+typedef Nodo *lista;
+
+void cargarArchivo(const char *nombreArchivo, lista *cabeza);
+void cantidadAnimales(lista cabeza, int cantidad);
+void imprimirLista(lista );
+int contarNodos(lista cabeza);
+void liberarListas(lista cabeza);
+
+
+
+
+
+#endif
